6ff19b1647431f72860adc7d824943fbc7f46849
AaryanW <83490486+AaryanW@users.noreply.github.com>
2023-11-16 08:36:15 -0800

    Update Roulette.py

--- Roulette.py
+++ Roulette.py
@@ -1,1 +1,71 @@
 #Roulette
+import random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def bet(str):
+#    if (str = "Straight Up"):
+#    if (str = "Split"):
+#    if (str = "Street"):
+#    if (str = "Corners"):
+#    if (str = "Six-Line Bet"):
+#    if (str = "Column"):
+#    if (str = "Dozen"):
+#    if (str = "Odd"):
+#    if (str = "Even"):
+#    if (str = "Red"):
+#    if (str = "Black"):
+#    if (str = "1-18"):
+#    if (str = "19-36"):    
+
+
+game_over = False
+
+balance = 1000
+while game_over == False:
+    # place holder
+    x = 0 
