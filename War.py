49045b29f5384af6b1f7e36a7e20933e2803ded7
munchingwolf <150168582+munchingwolf@users.noreply.github.com>
2023-12-04 08:49:17 -0800

    Update War.py

--- War.py
+++ War.py
@@ -3,21 +3,37 @@ import random
 import time
 from itertools import product
 
+#prints given string slowly
+#default delay of 0.1s if no delay peramter is given
+def slow_print(input_string, delay=None):
+    if delay is None:
+        delay = 0.02
+
+    for char in input_string:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
+
 # game over variable
 gameStatus = False
-print("Welcome to War! The goal of this game is to win all the cards in a deck. Each player starts out with exactly half the deck facedown.")
-print("At the same time, both players draw a singular card. The player with the highest value card wins, and takes all the cards drawn.")
-print("If the cards drawn hold the same value and the players tie, each player continues drawing cards until one person wins")
+slow_print("Welcome to War! The goal of this game is to win all the cards in a deck. Each player starts out with exactly half the deck facedown.")
+slow_print("At the same time, both players draw a singular card. The player with the highest value card wins, and takes all the cards drawn.")
+slow_print("If the cards drawn hold the same value and the players tie, each player continues drawing cards until one person wins")
 # mapping cards to numerical values
 
+#card combinations
 suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
 ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
+#shuffles deck
 deck = list(product(ranks, suits))
 random.shuffle(deck)
 
+#sets half the deck to player and computer
 index = 26
 playerDeck = deck[:26]
 computerDeck = deck[:26]
 
-#while playerDeck
+while playerDeck >= 0 :
+    slow_print("You have" + len(playerDeck) + "cards now and I" + len(computerDeck) + "cards") 
