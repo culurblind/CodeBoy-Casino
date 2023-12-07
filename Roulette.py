6a79f9061ef37ae679aeecb6fd018962581330ec
culurblind <parkerksmall@gmail.com>
2023-12-07 08:35:01 -0800

    Update Roulette.py

--- Roulette.py
+++ Roulette.py
@@ -178,6 +178,17 @@ while game_over == False:
                 print()
                 isInt = True
 
+    if typeOfBet == "Six-Line Bet":
+        isInt = False
+        while isInt == False:
+            slow_print("Which two lines would you like to bet on?")
+            slow_print("Use the top numbers as the labels for each line and make sure they lines are next to eachother? (EX: '12 15')")
+            print(''''3'   '6'   '9'   '12'  '15'  '18'  '21'  '24'  '27'  '30'  '33'  '36'
+ 2     5     8     11    14    17    20    23    26    29    32    35
+ 1     4     7     10    13    16    19    22    25    28    31    34
+                  ''')
+            SLB = input().split()
+
     if typeOfBet == "Row":
         isInt = False
         while isInt == False:
