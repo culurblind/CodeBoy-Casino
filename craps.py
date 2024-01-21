f3abade7c902abc6c104733d8eef4d768063a27f
munchingwolf <150168582+munchingwolf@users.noreply.github.com>
2024-01-20 23:03:31 -0800

    Update craps.py

--- craps.py
+++ craps.py
@@ -8,7 +8,7 @@ import os
 #default delay of 0.1s if no delay peramter is given
 def slow_print(input_string, delay=None):
     if delay is None:
-        delay = 0.001
+        delay = 0.05
 
     for char in input_string:
         print(char, end='', flush=True)
@@ -23,6 +23,26 @@ def clear_terminal():
 def isnt_int(raw_inp):
     if(not raw_inp.isdigit()):
         return True
+
+slow_print("welcome to Craps! Your goal is to see whether the dice will roll a winning number. In the first round,")
+slow_print("if you roll a 7 or 11, you win. If you roll a 2, 3, or 12, you automatically lose")
+slow_print("any other combination of values will be added to the point, and you will continue rerolling until you get that number again or roll a 7, in which you lose")
+time.sleep(1)
+clear_terminal()
+
+# Balance/ Betting set up
+balance = 1000
+slow_print("balance: " + str(balance))
+bet = input("How much do you want to bet? ")
+# makes sure the bet is an int
+while isnt_int(bet):
+    slow_print("You need to type a number as a valid bet")
+    time.sleep(1.5)
+    clear_terminal()
+    bet = input("How much do you want to bet? ")
+bet = int(bet)
+balance -= bet
+
 
 #rolls dice and adds them up
 def dicetotal():
@@ -43,27 +63,49 @@ def reRoll(diceNum):
             balance - 2 * bet
             pastNumber == True
         else:
-            diceNum= dicetotal()
-
-#runs game
-while gameplay == True:
-    diceNum = dicetotal()
-
-    nextStep = input("Type roll! ")
-    if nextStep == "roll":
-        slow_print("Rolling... Rolling... Rolling... ", 0.1)
-        if diceNum == 7 or diceNum == 11:
-            slow_print("The dice rolled " + str(diceNum) + ", You won!")
-            balance += 2 * bet
-            gameplay = False
-        elif diceNum == 2 or diceNum == 3 or diceNum == 12:
-            slow_print("You lost the bet, the dice rolled " + str(diceNum))
-            gameplay = False
+            slow_print("You rolled ," + str(newNum) + "the dice will roll again")
+def playgame():
+    #runs game
+    while gameplay == True:
+        diceNum = dicetotal()
+        nextStep = input("Type roll! ")
+        if nextStep == "roll":
+            slow_print("Rolling... Rolling... Rolling... ", 0.1)
+            if diceNum == 7 or diceNum == 11:
+                slow_print("The dice rolled " + str(diceNum) + ", You won!")
+                balance += 2 * bet
+                gameplay = False
+            elif diceNum == 2 or diceNum == 3 or diceNum == 12:
+                slow_print("You lost the bet, the dice rolled " + str(diceNum))
+                gameplay = False
+            else:
+                list.append(diceNum)
+                print("you rolled " + str(diceNum) + " the die will reroll")
+                #true/flase for if a number is rerolled
+                pastNumber = False
+                reRoll(diceNum)
+                gameplay == False
         else:
-            list.append(diceNum)
-            #true/flase for if a number is rerolled
-            pastNumber = False
-            reRoll(diceNum)
-            gameplay == False
-    else:
-        print("You did not type roll")
+            print("You did not type roll")
+playTrue = True
+numGames = 1
+gameplay = True
+list = []
+nextnumber = []
+
+#asks the user if they want to keep playing
+while playTrue == True:
+    if numGames == 1:
+        status = input("Do you want to play the game?")
+        if status == "Yes" or "yes":
+            playgame(gameplay)
+            numGames += 1
+        else:
+            playTrue = False
+    if numGames > 1:
+        status = input("Do you want to play again?")
+        if status == "Yes" or "yes":
+            playgame()
+            numGames += 1
+        else:
+            playTrue = False
