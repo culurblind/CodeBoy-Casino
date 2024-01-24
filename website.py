762f73dd71cc81e204f4c22264d09576f1b4a9a5
AaryanW <83490486+AaryanW@users.noreply.github.com>
2024-01-23 23:52:06 -0800

    Update website.py

--- website.py
+++ website.py
@@ -10,13 +10,13 @@ def home():
 def roulette():
     return render_template('roulette.html')
 
-#@app.route('/blackjack')
-#def blackjack():
-    #return render_template('blackjack.html')
+@app.route('/blackjack')
+def blackjack():
+    return render_template('blackjack.html')
 
-#@app.route('/craps')
-#def craps():
-    #return render_template('craps.html')
+@app.route('/craps')
+def craps():
+    return render_template('craps.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
