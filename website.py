18d61ec3c189ee80555f7133cff0f51d73fef51a
AaryanW <83490486+AaryanW@users.noreply.github.com>
2024-01-24 10:24:13 -0800

    1

--- website.py
+++ website.py
@@ -14,19 +14,16 @@ def home():
 @app.route('/roulette')
 def roulette():
     result = rl.roulette(balance)
-    balance = result
     return render_template('roulette.html', result = result)
 
 @app.route('/blackjack')
 def blackjack():
     result = bj.blackJack(balance)
-    balance = result
     return render_template('blackjack.html', result = result)
 
 @app.route('/craps')
 def craps():
     result = cr.craps(balance)
-    balance = result
     return render_template('craps.html', result = result)
 
 # We need to make code to make the balance accesible by the JS file
