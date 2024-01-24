0b59f8bfaa5dd111f3e5577fec47c7d14ced46f2
Parker <parkerksmall@gmail.com>
2024-01-24 03:05:17 -0800

    pop

--- website.py
+++ website.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 import Roulette as rl
 import Craps as cr
 import BlackJack as bj
@@ -13,21 +14,21 @@ def home():
 
 @app.route('/roulette')
 def roulette():
-    result = rl.roulette(balance)
-    balance = result
-    return render_template('roulette.html', result = result)
+    #result = rl.roulette(balance)
+    #balance = result
+    return render_template('roulette.html')#, result = result)
 
 @app.route('/blackjack')
 def blackjack():
-    result = bj.blackJack(balance)
-    balance = result
-    return render_template('blackjack.html', result = result)
+    #result = bj.blackJack(balance)
+    #balance = result
+    return render_template('blackjack.html')#, result = result)
 
 @app.route('/craps')
 def craps():
-    result = cr.craps(balance)
-    balance = result
-    return render_template('craps.html', result = result)
+    #result = cr.craps(balance)
+    #balance = result
+    return render_template('craps.html')#, result = result)
 
 # We need to make code to make the balance accesible by the JS file
 #@app.route('/')
