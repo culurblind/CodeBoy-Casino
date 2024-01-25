7da8bcaabd0ea5e9bf1cd643c60139d0a6f2ce60
AaryanW <83490486+AaryanW@users.noreply.github.com>
2024-01-25 01:48:22 -0800

    12

--- website.py
+++ website.py
@@ -1,6 +1,5 @@
 # website.py is the main code to connect the back end code with the front end code and open a local link for the website
 
-from flask import Flask, render_template
 import programs.Roulette as rl
 import programs.Craps as cr
 import programs.BlackJack as bj
@@ -13,6 +12,11 @@ balance = 1000
 def home():
     return render_template('home.html')
 
+# for back button
+@app.route('/home')
+def returnHome():
+    return render_template('home.html')
+
 @app.route('/roulette')
 def roulette():
     # Call the function/method that runs the roulette game
@@ -22,7 +26,7 @@ def roulette():
 @app.route('/blackjack')
 def blackjack():
     # Call the function/method that runs the blackjack game
-    result = bj.blackjack(balance)
+    result = bj.blackJack(balance)
     return render_template('blackjack.html', result=result)
 
 @app.route('/craps')
@@ -30,6 +34,10 @@ def craps():
     # Call the function/method that runs the craps game
     result = cr.craps(balance)
     return render_template('craps.html', result=result)
+
+@app.route('/get_balance', methods=['GET'])
+def get_balance():
+    return jsonify({'balance': balance})
 
 if __name__ == '__main__':
     app.run(debug=True)
